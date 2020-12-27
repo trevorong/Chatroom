6bf7ca38daac43504bf1eca8a2bbe25928b2e52f
Trevor <tr89on@gmail.com>
2020-12-27 15:27:19 -0800

    added a navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import ChatModule from './ChatModule.js';
 import LoginModule from './LoginModule.js';
 
 function App() {
   return (
     <Router>
+      <nav id="main-navbar" className="navbar navbar-dark bg-primary">
+        <div className="container-fluid">
+          <Link to= "/" className="navbar-brand">uMessage</Link>
+        </div>
+      </nav>
       <div className="App">
         <Switch>
           <Route path="/" exact component={LoginModule} />
--- src/LoginModule.js
+++ src/LoginModule.js
@@ -8,7 +8,7 @@ function LoginModule() {
 
   return (
     <div className="login-module border border-primary rounded">
-      <h1> uMessage </h1>
+      <h1> Enter a chatroom: </h1>
       <input type="text" className="form-control" placeholder="Username" onChange={(event)=>setNameVal(event.target.value)} value={nameVal} aria-label="Username"></input>
       <input type="text" className="form-control" placeholder="Password" onChange={(event)=>setPassword(event.target.value)} value={password} aria-label="Password"></input>
       <Link to= "/room" className="btn btn-primary">Enter a room</Link>
