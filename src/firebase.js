17a0f103c74d29f0eee5dbbd735eb8f37699e0ad
Trevor <tr89on@gmail.com>
2020-12-27 17:00:21 -0800

    added initial firebase support

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBtiKOxaGzznFs0IjxXKfZGrdpFKYnkwVw",
+    authDomain: "chatroom-fc335.firebaseapp.com",
+    projectId: "chatroom-fc335",
+    storageBucket: "chatroom-fc335.appspot.com",
+    messagingSenderId: "393282248112",
+    appId: "1:393282248112:web:6cf0540c5c76e88d9e8a75"
+};
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+export { db };
